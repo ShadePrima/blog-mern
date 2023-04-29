f3caba74545c2785265003ba30a8ec6dbba82f08
ShadePrima <aleksandrag1984@gmail.com>
2023-04-29 21:00:16 +0300

    get one article

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,5 +1,51 @@
 import PostModel from '../models/Post.js'
 
+export const getAll = async (req, res) => {
+  try {
+    const posts = await PostModel.find().populate('user').exec()
+
+    res.json(posts)
+  } catch (err) {
+    console.log(err)
+
+    res.status(500).json({
+      message: 'Article failed',
+    })
+  }
+}
+
+export const getOne = async (req, res) => {
+  try {
+    const postId = req.params.id
+
+    PostModel.findOneAndUpdate(
+      {
+        _id: postId,
+      },
+      {
+        $inc: { viewsCount: 1 },
+      },
+      {
+        returnDocument: 'after',
+      }
+    )
+      .then((doc) => {
+        res.json(doc)
+      })
+      .catch((err) => {
+        console.log(err)
+        return res.status(500).json({
+          message: 'Could not return article',
+        })
+      })
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({
+      message: 'Article failed',
+    })
+  }
+}
+
 export const create = async (req, res) => {
   try {
     const doc = new PostModel({
